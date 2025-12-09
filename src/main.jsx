186a65db904a1b5fdc67c14baa8d406e699f676c
Yashitha <yashitha618@gmail.com>
2025-12-09 20:44:11 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Shop from "./pages/Shop.jsx";   // <- your component
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Shop />
+  </React.StrictMode>
+);
+
